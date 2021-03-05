f716f3ae1dcd3be72d3fd97167c742a8083eef77
Bruno DELATTRE <delattreb@gmail.com>
2021-03-05 21:34:18 +0100

    Clock Ok Animation OK

--- config.h
+++ config.h
@@ -11,10 +11,13 @@
 #define ATTENPTING 1000
 
 //LED configuration
-//#define LED_PIN_24 2
+#define LED_PIN_24 5
 #define LED_PIN_60 4
-//#define LED_COUNT_24 24
+#define LED_COUNT_24 24
 #define LED_COUNT_60 60
+#define SPEED_EFFECT 1400
+#define BRIGHTNESS_60 15
+#define BRIGHTNESS_24 65
 
 #pragma region IPDEFINITION
 #pragma endregion
